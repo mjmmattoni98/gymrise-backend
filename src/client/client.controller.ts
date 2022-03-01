c95c9ed14cb415f9bf991c620d1c0abcf005f611
al376994 <al376994@uji.es>
2022-03-01 17:01:55 +0100

    el POST de createClient pide lo necesario en el BODY

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -18,9 +18,32 @@ export class ClientController {
 
   @Post('user')
   @UseGuards(JwtAuthGuard)
-  async signupUser(
-    @Body() userData: { name?: string; email: string },
-  ): Promise<ClientModel> {
+  async signupUser( @Body() userData: {
+      name: String,
+      surname: String,
+      dni: String,
+      password: tring,
+      email: string,
+      description: string,
+      height: Int,
+
+
+    } ): Promise<ClientModel> {
     return this.clientService.createClient(userData);
   }
 }
+
+
+name                    String                    @db.VarChar(40)
+  surname                 String                    @db.VarChar(40)
+  dni                     String                    @id(map: "client_pk") @db.VarChar(9)
+  password                String                    @db.VarChar(30)
+  email                   String                    @db.VarChar(40)
+  description             String
+  height                  Int
+  weight                  Int
+  sex                     sex
+  age                     Int
+  chat                    chat[]
+  contract                contract[]
+  training_session_client training_session_client[]
