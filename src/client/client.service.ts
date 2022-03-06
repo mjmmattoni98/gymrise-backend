4b935e5e12e9aa1e37980d8176c2695bfb6309f8
mjmmattoni98 <110f6a4b2d9075c1c55d946149aa67a82dccd5f9>
2022-03-06 20:42:20 +0100

    minimal change

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -3,23 +3,25 @@ import { PrismaService } from '../prisma.service';
 import { client, Prisma } from '@prisma/client';
 
 export enum ClientCreationError {
-  ClientAlreadySignedUp
+  ClientAlreadySignedUp,
 }
 
 @Injectable()
 export class ClientService {
   constructor(private prisma: PrismaService) {}
 
-  async client(clientWhereUniqueInput: Prisma.clientWhereUniqueInput): Promise<client | null> {
+  async client(
+    clientWhereUniqueInput: Prisma.clientWhereUniqueInput,
+  ): Promise<client | null> {
     return this.prisma.client.findUnique({ where: clientWhereUniqueInput });
   }
 
   async clients(params: {
-    skip?: number,
-    take?: number,
-    cursor?: Prisma.clientWhereUniqueInput,
-    where?: Prisma.clientWhereInput,
-    orderBy?: Prisma.clientOrderByWithRelationInput
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.clientWhereUniqueInput;
+    where?: Prisma.clientWhereInput;
+    orderBy?: Prisma.clientOrderByWithRelationInput;
   }): Promise<client[]> {
     const { skip, take, cursor, where, orderBy } = params;
     return this.prisma.client.findMany({
@@ -27,12 +29,14 @@ export class ClientService {
       take,
       cursor,
       where,
-      orderBy
+      orderBy,
     });
   }
 
   async createClient(data: Prisma.clientCreateInput): Promise<client> {
-    const foundClient = await this.prisma.client.findUnique({ where: {dni: data.dni} });
+    const foundClient = await this.prisma.client.findUnique({
+      where: { dni: data.dni },
+    });
     if (foundClient != null) {
       throw ClientCreationError.ClientAlreadySignedUp;
     }
@@ -40,8 +44,8 @@ export class ClientService {
   }
 
   async updateClient(params: {
-    where: Prisma.clientWhereUniqueInput,
-    data: Prisma.clientUpdateInput
+    where: Prisma.clientWhereUniqueInput;
+    data: Prisma.clientUpdateInput;
   }): Promise<client> {
     const { where, data } = params;
     return this.prisma.client.update({ data, where });
