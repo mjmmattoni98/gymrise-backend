4100f198d03b51a0575d986fd26b6c0c9a6f3ec6
marcos <javier.magni@gmail.com>
2022-03-07 16:17:36 +0100

    Added API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Gymrise API')
+    .setDescription('The Gymrise API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   // binds ValidationPipe to the entire application
   app.useGlobalPipes(new ValidationPipe());
 
