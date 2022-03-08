6f52e5154ac07ab3ed4836009df9fe8700f1aa54
AlejandroGB13 <al386123@uji.es>
2022-03-08 16:43:02 +0100

    delete, update (trainer-falta)

--- src/personal-trainer/personal-trainer.controller.ts
+++ src/personal-trainer/personal-trainer.controller.ts
@@ -5,18 +5,18 @@ import {
   Post,
   Body,
   Put,
-  Delete, UseGuards, HttpException, HttpStatus
+  Delete, UseGuards, HttpException, HttpStatus, Patch
 } from "@nestjs/common";
 import { PersonalTrainerService } from './personal-trainer.service';
 import { PersonalTrainerCreationError } from "./personal-trainer.service";
 import { PersonalTrainerDto } from './dto/personalTrainer.dto';
-import { client as ClientModel, personal_trainer as PersonalTrainerModel } from "@prisma/client";
+import { client as ClientModel, personal_trainer as PersonalTrainerModel, Prisma } from "@prisma/client";
 
 @Controller('personal-trainer')
 export class PersonalTrainerController {
   constructor(private readonly personalTrainerService: PersonalTrainerService) {}
 
-  @Post('trainer')
+  @Post('add')
   async signupTrainer( @Body() trainerData: PersonalTrainerDto): Promise<PersonalTrainerModel>
   {
     try {
@@ -31,15 +31,25 @@ export class PersonalTrainerController {
     }
   }
 
-  @Delete('trainer')
-  async cancelTrainerAccount( @Body() trainerData: PersonalTrainerDto ): Promise<PersonalTrainerModel> {
+  @Delete('delete/:dni')
+  async cancelTrainerAccount( @Param ('dni') dni: string ): Promise<PersonalTrainerModel> {
 
     try {
-      return await this.personalTrainerService.deletePersonalTrainer(trainerData);
+      return await this.personalTrainerService.deletePersonalTrainer(dni as Prisma.personal_trainerWhereUniqueInput);
     } catch (error) {
       throw new Error("Error while deleting the personal trainer account");
     }
 
   }
 
+  @Put('update/:dni')
+  async updateTrainerAccount(@Param ('dni') dni: string,
+                             @Body() trainerData: PersonalTrainerDto ): Promise<PersonalTrainerModel> {
+    try{
+      return await this.personalTrainerService.updatePersonalTrainer({where:dni as Prisma.personal_trainerWhereUniqueInput
+        , data:trainerData})
+    }catch (error){
+      throw new Error("Error while updating the personal trainer account")
+    }
+  }
 }
