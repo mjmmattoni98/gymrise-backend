8e55e1855bbac7a7f7c5aa0b10fb89b8f4b2c5dd
Alejandro <al386123@uji.es>
2022-03-19 13:17:45 +0100

    training_session delete and update

--- src/training-session/training-session.service.ts
+++ src/training-session/training-session.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { training_session, Prisma } from '@prisma/client';
+import { training_session, Prisma, personal_trainer } from "@prisma/client";
 
 @Injectable()
 export class TrainingSessionService {
@@ -10,5 +10,20 @@ export class TrainingSessionService {
     data: Prisma.training_sessionCreateInput,
   ): Promise<training_session> {
     return this.prisma.training_session.create({ data });
+  }
+
+  async deleteTrainingSession(id: number): Promise<training_session> {
+    return this.prisma.training_session.delete({ where: { id: id } });
+  }
+
+  async updateTrainingSession(params: {
+    id: number;
+    data: Prisma.training_sessionUpdateInput;
+  }): Promise<training_session> {
+    const { data, id } = params;
+    return this.prisma.training_session.update({
+      data: data,
+      where: { id: id },
+    });
   }
 }
