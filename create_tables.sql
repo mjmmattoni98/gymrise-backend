d878763cefc7da89b319ba3ef4331b36acbaaaa9
mjmmattoni98 <javier.magni@gmail.com>
2022-05-01 23:45:41 +0200

    updated database

--- create_tables.sql
+++ create_tables.sql
@@ -15,7 +15,7 @@ CREATE TABLE training_skill (
     dni VARCHAR(9) NOT NULL,
     skill SKILL NOT NULL,
     CONSTRAINT training_skills_pk PRIMARY KEY (dni, skill),
-    CONSTRAINT training_skills_fk FOREIGN KEY (dni) REFERENCES personal_trainer (dni) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT training_skills_fk FOREIGN KEY (dni) REFERENCES personal_trainer (dni) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE client (
@@ -40,8 +40,8 @@ CREATE TABLE chat (
     date_time TIMESTAMP NOT NULL,
     text TEXT NOT NULL,
     CONSTRAINT chat_pk PRIMARY KEY (dni_trainer, dni_client, date_time),
-    CONSTRAINT chat_fk_trainer FOREIGN KEY (dni_trainer) REFERENCES personal_trainer (dni)  ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT chat_fk_client FOREIGN KEY (dni_client) REFERENCES client (dni) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT chat_fk_trainer FOREIGN KEY (dni_trainer) REFERENCES personal_trainer (dni) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT chat_fk_client FOREIGN KEY (dni_client) REFERENCES client (dni) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE contract (
@@ -54,19 +54,20 @@ CREATE TABLE contract (
     price INTEGER NOT NULL,
     accepted BOOLEAN NOT NULL DEFAULT FALSE,
     CONSTRAINT contract_pk PRIMARY KEY (id),
-    CONSTRAINT contract_fk_trainer FOREIGN KEY (dni_trainer) REFERENCES personal_trainer (dni) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT contract_fk_client FOREIGN KEY (dni_client) REFERENCES client (dni) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT contract_fk_trainer FOREIGN KEY (dni_trainer) REFERENCES personal_trainer (dni) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT contract_fk_client FOREIGN KEY (dni_client) REFERENCES client (dni) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT contract_price_ch CHECK (price >= 0)
 );
 
 CREATE TABLE training_session (
     id SERIAL,
+    title VARCHAR(40) NOT NULL,
     date_time TIMESTAMP NOT NULL,
     dni VARCHAR(9) NOT NULL,
     description TEXT NOT NULL,
     price INTEGER NOT NULL,
     CONSTRAINT training_session_pk PRIMARY KEY (id),
-    CONSTRAINT training_session_fk_trainer FOREIGN KEY (dni) REFERENCES personal_trainer (dni) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT training_session_fk_trainer FOREIGN KEY (dni) REFERENCES personal_trainer (dni) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT training_session_price_ch CHECK (price >= 0)
 );
 
@@ -74,8 +75,8 @@ CREATE TABLE training_session_client (
     id INTEGER NOT NULL,
     dni VARCHAR(9) NOT NULL,
     CONSTRAINT training_session_client_pk PRIMARY KEY (id, dni),
-    CONSTRAINT training_session_client_fk_client FOREIGN KEY (dni) REFERENCES client (dni) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT training_session_client_fk_session FOREIGN KEY (id) REFERENCES training_session (id) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT training_session_client_fk_client FOREIGN KEY (dni) REFERENCES client (dni) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT training_session_client_fk_session FOREIGN KEY (id) REFERENCES training_session (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
--- inserts.sql
+++ inserts.sql
@@ -39,10 +39,10 @@ INSERT INTO contract (dni_trainer, dni_client, description, start_date, end_date
 INSERT INTO contract (dni_trainer, dni_client, description, start_date, end_date, price) VALUES ('11111111A', '11111111C', 'string', '2022-05-08 18:00:00', '2022-06-08 18:00:00', 0);
 INSERT INTO contract (dni_trainer, dni_client, description, start_date, end_date, price) VALUES ('11111111B', '11111111C', 'string', '2022-05-08 18:00:00', '2022-06-08 18:00:00', 10);
 
-INSERT INTO training_session (date_time, dni, description, price) VALUES ('2022-05-08 18:20:00', '11111111A', 'string', 0);
-INSERT INTO training_session (date_time, dni, description, price) VALUES ('2022-05-18 18:20:00', '11111111A', 'string', 10);
-INSERT INTO training_session (date_time, dni, description, price) VALUES ('2022-05-08 18:20:00', '11111111B', 'string', 10);
-INSERT INTO training_session (date_time, dni, description, price) VALUES ('2022-05-08 18:20:00', '11111111C', 'string', 10);
+INSERT INTO training_session (title, date_time, dni, description, price) VALUES ('Clase de aerobic', '2022-05-08 18:20:00', '11111111A', 'string', 0);
+INSERT INTO training_session (title, date_time, dni, description, price) VALUES ('Clase de fuerza', '2022-05-18 18:20:00', '11111111A', 'string', 10);
+INSERT INTO training_session (title, date_time, dni, description, price) VALUES ('Clase de estiramientos', '2022-05-08 18:20:00', '11111111B', 'string', 10);
+INSERT INTO training_session (title, date_time, dni, description, price) VALUES ('Clase de zumba principiantes', '2022-05-08 18:20:00', '11111111C', 'string', 10);
 
 INSERT INTO training_session_client VALUES (1, '11111111A');
 INSERT INTO training_session_client VALUES (2, '11111111A');
